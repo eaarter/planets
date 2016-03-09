927af3a42d693d10bc70ceafefe20519cea04b8d
Elizabeth Arter <eaa25@bath.ac.uk>
2016-03-09 14:16:23 +0000

    edit to file to delete2

--- func.f90
+++ func.f90
@@ -2,7 +2,7 @@ function func(x) result (func_result)
 %random stuff     
 real(kind=8), intent(in) :: x
 real(kind=8) :: func_result
-
+%more random stuff
 if (abs(x) <= 1.0_8) then
   func_result = sqrt(1.0_8-x**2)
 else
