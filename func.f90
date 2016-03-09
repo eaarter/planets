3b0b0c9a26dbe2645dcf35f183bc0bd6f515e0fc
Elizabeth Arter <eaa25@bath.ac.uk>
2016-03-09 13:46:02 +0000

    add first function file

---
+++ func.f90
@@ -0,0 +1,13 @@
+function func(x) result (func_result)
+     
+real(kind=8), intent(in) :: x
+real(kind=8) :: func_result
+
+if (abs(x) <= 1.0_8) then
+  func_result = sqrt(1.0_8-x**2)
+else
+  print*, x,'not in domain of func(x). Program terminated.'
+  stop
+end if
+
+end function func
